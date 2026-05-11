bba41b4ffcf0db64baab62637199cbabec3ec9b8
Stefan Mallaun <e12519634@student.tuwien.ac.at>
2026-05-12 00:24:30 +0200

    test

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ if (demoButton) {
         alert("Vielen Dank für Ihr Interesse an LogiFlow!");
     });
 }
+
+const toggleButton = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggleButton.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+    const expanded =
+        toggleButton.getAttribute("aria-expanded") === "true";
+
+    toggleButton.setAttribute(
+        "aria-expanded",
+        !expanded
+    );
+});
